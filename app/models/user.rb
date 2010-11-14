12fb69c419133031fe17566e6a8907dca6ab73f7
Michael Economy <michael@michaeleconomy.com>
2010-11-13 18:41:49 -0800

    typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_many :authorizations
   has_many :comments, :dependent => :destroy
   has_many :ratings, :dependent => :destroy
-  has_many :sites_created, :classname => 'Site', :foreign_key => 'creator_user_id'
+  has_many :sites_created, :class_name => 'Site', :foreign_key => 'creator_user_id'
 end
