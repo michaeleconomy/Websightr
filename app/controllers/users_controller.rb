50c8dc9ee5c6bc29839487e466b360a6978fe859
Michael Economy <michael@michaeleconomy.com>
2010-11-17 22:42:26 -0800

    fix user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   
   
   def show
-    
+    @sites = @user.sites_created.all :limit => 10
+    @comments = @user.comments.all :limit => 10
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,15 @@ Websightr::Application.routes.draw do
       post 'approve'
     end
   end
-  resources :ratings, :comments, :users
+  
+  resources :ratings, :comments
+  
+  resources :users do
+    member do
+      get 'sites', :to => 'sites#user'
+      get 'comments', :to => 'comments#user'
+    end
+  end
   
   match '/auth/:provider/callback', :to => 'sessions#create'
   match '/session/fake_sign_in/:id', :to => 'sessions#fake_sign_in'
